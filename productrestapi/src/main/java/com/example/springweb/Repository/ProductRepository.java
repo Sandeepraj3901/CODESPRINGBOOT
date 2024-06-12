2321aecc710f5f283f7d446750850a2dfd98cde7
Sandeep Raj <33432741+Sandeepraj3901@users.noreply.github.com>
2024-06-12 16:46:33 -0500

    Delete productrestapi directory
    sdf

--- productrestapi/src/main/java/com/example/springweb/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springweb.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springweb.entites.products;
-
-public interface ProductRepository extends JpaRepository<products, Integer > {
-
-}
